e6c2119f2b1ea9d2e16ac8070bcfb61558e9c892
Pekal Shmekal <veryimportantbucket@gmail.com>
2016-11-25 03:19:15 +0100

    - remove duplicate dependency in Gemfile - remove all non-rspec test-files(scripts) - rename spec/basic_spec.rb to spec/register_spec.rb and move it to spec/features/ - most important: reorganize register_spec.rb and respective class   note: not sure if split checks by register is worth doing at all as register of initial query affects nothing - register of Title and Description depends only on the particular search result and URL is always downcase

--- lib/page.rb
+++ lib/page.rb
@@ -1,8 +1,9 @@
 require 'capybara'
 require 'capybara/dsl'
+require 'capybara/rspec'
 require 'uri'
 
-# workaround to keep browser alive for debugging after test completion
+# # workaround to keep browser alive for debugging after test completion
 # Capybara::Selenium::Driver.class_eval do
 #   def quit
 #     puts "Press RETURN to quit the browser"
@@ -20,16 +21,10 @@ class Page
     Capybara.current_driver = :selenium
   end
 
-  def enter_keyword(keyword, params={})
-    # also can be executed here or in the initialize method
-    # visit 'https://google.com'
+  def enter_keyword(keyword)
     fill_in 'q', with: keyword
-    locator = params[:im_feeling_lucky] ? '.sbsb_i.sbqs_b' : '.lsb'
-    find(locator, match: :first).click
-  end
-
-  def parse_first_result
-    find('.g', match: :first).text.downcase
+    # locator = params[:im_feeling_lucky] ? '.sbsb_i.sbqs_b' : '.lsb'
+    find('.sbico', match: :first).click
   end
 
   def parse_all_sites
@@ -37,6 +32,36 @@ class Page
       site_list = []
       all('._Rm').each { |i| site_list << i.text }
       site_list
+    end
+  end
+
+  def first_result_title
+    parse_first_result(:title)
+  end
+
+  def first_result_url
+    parse_first_result(:url)
+  end
+
+  def first_result_description
+    parse_first_result(:description)
+  end
+
+  private
+
+  def parse_first_result(destination)
+    locator =
+      case destination
+        when :title
+          '.r'
+        when :url
+          '.f'
+        when :description
+          '.st'
+      end
+
+    within(find('.rc', match: :first)) do
+      find(locator).text
     end
   end
 
---
+++ spec/features/register_spec.rb
@@ -0,0 +1,55 @@
+# spec/basic_spec.rb
+
+require 'page'
+
+
+RSpec.feature "Verify basic query search results" do
+
+  before do
+    @page = Page.new
+    @page.visit 'https://google.com'
+  end
+
+  feature "Upcase input" do
+
+    # not sure if curly bracket syntax is acceptable
+    before { @page.enter_keyword 'Monster' }
+
+    feature "First result" do
+      scenario "Title contains inputted keyword Upcase" do
+        expect(@page.first_result_title).to include('Monster')
+      end
+
+      scenario "URL contains inputted keyword Downcase" do
+        expect(@page.first_result_url).to include('monster')
+      end
+
+      scenario "Description contains inputted keyword Upcase" do
+        expect(@page.first_result_description).to include('Monster')
+      end
+    end
+
+  end
+
+
+  feature "Downcase input" do
+
+    before { @page.enter_keyword 'monster' }
+
+    feature "First result" do
+      scenario "Title contains inputted keyword Upcase" do
+        expect(@page.first_result_title).to include('Monster')
+      end
+
+      scenario "URL contains inputted keyword Downcase" do
+        expect(@page.first_result_url).to include('monster')
+      end
+
+      scenario "Description contains inputted keyword Upcase" do
+        expect(@page.first_result_description).to include('Monster')
+      end
+    end
+
+  end
+
+end
