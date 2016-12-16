e19a8b0b6e572fd39a190e309980430e5ac3506e
Pekal Shmekal <veryimportantbucket@gmail.com>
2016-12-16 04:04:18 +0100

    - undo previous 'spec split into two', namely combine two files into one. - extend scenarios amount with cyryllic and multi-word keyword cases and revise their structure. - introduce given, background and subject(not an alias but also implemented) capybara aliases. - Gemfile.lock update due to bundler update process.

--- lib/page.rb
+++ lib/page.rb
@@ -55,7 +55,7 @@ class Page
         when :title
           '.r'
         when :url
-          '.f'
+          '.f.kv'
         when :description
           '.st'
       end
---
+++ spec/features/basic_search_spec.rb
@@ -0,0 +1,110 @@
+# spec/register_down_spec.rb
+
+require 'page'
+
+RSpec.feature "Verify query search results" do
+
+  subject { Page.new }
+
+  background do
+    subject.visit 'https://google.com'
+    subject.enter_keyword keyword
+  end
+
+  feature "Use one-word keyword" do
+
+    context "Ignore case" do
+
+      context "Downcase input" do
+
+        given(:keyword) {'monster'}
+
+        scenario "Title contains inputted keyword Upcase" do
+          expect(subject.first_result_title).to include(keyword.capitalize)
+        end
+
+        scenario "URL contains inputted keyword Downcase" do
+          expect(subject.first_result_url).to include(keyword.downcase)
+        end
+
+        scenario "Description contains inputted keyword Upcase" do
+          expect(subject.first_result_description).to include(keyword.capitalize)
+        end
+
+      end # context downcase
+
+
+      context "Upcase input" do
+
+        given(:keyword) {'Monster'}
+
+        scenario "Title contains inputted keyword Upcase" do
+          expect(subject.first_result_title).to include(keyword.capitalize)
+        end
+
+        scenario "URL contains inputted keyword Downcase" do
+          expect(subject.first_result_url).to include(keyword.downcase)
+        end
+
+        scenario "Description contains inputted keyword Upcase" do
+          expect(subject.first_result_description).to include(keyword.capitalize)
+        end
+
+      end # context upcase
+
+
+      context "Mixed case input" do
+
+        given(:keyword) {'mONSteR'}
+
+        scenario "Title contains inputted keyword Upcase" do
+          expect(subject.first_result_title).to include(keyword.capitalize)
+        end
+
+        scenario "URL contains inputted keyword Downcase" do
+          expect(subject.first_result_url).to include(keyword.downcase)
+        end
+
+        scenario "Description contains inputted keyword Upcase" do
+          expect(subject.first_result_description).to include(keyword.capitalize)
+        end
+
+      end # context mixed
+
+    end # context ignore
+
+
+    context "cyryllic symbols" do
+
+      given(:keyword) {'Монстр'}
+
+      scenario "Title contains inputted cyryllic keyword" do
+        expect(subject.first_result_title).to include(keyword.capitalize)
+      end
+
+      scenario "Description contains inputted cyryllic keyword" do
+        expect(subject.first_result_description).to include(keyword.capitalize)
+      end
+
+    end
+
+  end # feature one-word
+
+
+  feature "Use multi-word keyword" do
+
+    given(:keyword) {'Buy Car Online'}
+
+    before { subject.enter_keyword 'Buy Car Online' }
+
+    scenario "Title contains inputted keyword" do
+      expect(subject.first_result_title).to include('Buy', 'Car', 'Online')
+    end
+
+    scenario "Description contains inputted keyword" do
+      expect(subject.first_result_description).to include('buy', 'car', 'online')
+    end
+
+  end
+
+end # feature verify search
