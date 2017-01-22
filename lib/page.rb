000d10df061d8026d8158e066e6cedad9e6b5143
Pekal Shmekal <veryimportantbucket@gmail.com>
2017-01-22 19:19:15 +0100

    Fixes as per PR comments.

--- lib/page.rb
+++ lib/page.rb
@@ -17,7 +17,7 @@ require 'site_prism'
 
 class SearchFieldSection < SitePrism::Section
   element :text_input, '#lst-ib'
-  element :voice_imput, '.gsri_a'
+  element :voice_input, '.gsri_a'
   element :search_button, '.sbico'
 end
 
@@ -25,7 +25,6 @@ class SearchResultSection < SitePrism::Section
   element :title, '.r'
   element :url, '.f.kv'
   element :description, '.st'
-
 end
 
 class Page < SitePrism::Page
@@ -33,6 +32,10 @@ class Page < SitePrism::Page
 
   def initialize
     Capybara.current_driver = :selenium
+  end
+
+  def navigate_to_home_page
+    self.load
   end
 end
 
@@ -44,59 +47,16 @@ end
 class SearchResults < Page
   section :search_field, SearchFieldSection, '.sbibod'
   sections :search_results, SearchResultSection, '.rc'
+
+  def first_result_title
+    search_results.first.title.text
+  end
+
+  def first_result_url
+    search_results.first.url.text
+  end
+
+  def first_result_description
+    search_results.first.description.text
+  end
 end
-
-
-
-# class Page
-#   include Capybara::DSL
-
-#   def initialize
-#     Capybara.current_driver = :selenium
-#   end
-
-#   def enter_keyword(keyword)
-#     fill_in 'q', with: keyword
-#     # locator = params[:im_feeling_lucky] ? '.sbsb_i.sbqs_b' : '.lsb'
-#     find('.sbico', match: :first).click
-#   end
-
-#   def parse_all_sites
-#     within('.srg') do
-#       site_list = []
-#       all('._Rm').each { |i| site_list << i.text }
-#       site_list
-#     end
-#   end
-
-#   def first_result_title
-#     parse_first_result(:title)
-#   end
-
-#   def first_result_url
-#     parse_first_result(:url)
-#   end
-
-#   def first_result_description
-#     parse_first_result(:description)
-#   end
-
-#   private
-
-#   def parse_first_result(destination)
-#     locator =
-#       case destination
-#         when :title
-#           '.r'
-#         when :url
-#           '.f.kv'
-#         when :description
-#           '.st'
-#       end
-
-#     within(find('.rc', match: :first)) do
-#       find(locator).text
-#     end
-#   end
-
-# end
--- spec/features/basic_search_spec.rb
+++ spec/features/basic_search_spec.rb
@@ -4,14 +4,9 @@ require 'page'
 
 RSpec.feature "Verify query search results" do
 
-  # subject { Page.new }
-  # subject { Home.new }
-
   background do
-    # subject.visit 'https://google.com'
-    # subject.enter_keyword keyword
-  @page = Home.new
-    @page.load
+    @page = Home.new
+    @page.navigate_to_home_page
     @page.wait_for_search_field(3)
     @page.search_field.text_input.set keyword
     @page.search_field.search_button.click
@@ -25,15 +20,15 @@ RSpec.feature "Verify query search results" do
         given(:keyword) {'monster'}
 
         scenario "Title contains inputted keyword Upcase" do
-          expect(@page.search_results.first.title.text).to include(keyword.capitalize)
+          expect(@page.first_result_title).to include(keyword.capitalize)
         end
 
         scenario "URL contains inputted keyword Downcase" do
-          expect(@page.search_results.first.url.text).to include(keyword.downcase)
+          expect(@page.first_result_url).to include(keyword.downcase)
         end
 
         scenario "Description contains inputted keyword Upcase" do
-          expect(@page.search_results.first.description.text).to include(keyword.capitalize)
+          expect(@page.first_result_description).to include(keyword.capitalize)
         end
       end # context downcase
 
@@ -42,15 +37,15 @@ RSpec.feature "Verify query search results" do
         given(:keyword) {'Monster'}
 
         scenario "Title contains inputted keyword Upcase" do
-          expect(@page.search_results.first.title.text).to include(keyword.capitalize)
+          expect(@page.first_result_title).to include(keyword.capitalize)
         end
 
         scenario "URL contains inputted keyword Downcase" do
-          expect(@page.search_results.first.url.text).to include(keyword.downcase)
+          expect(@page.first_result_url).to include(keyword.downcase)
         end
 
         scenario "Description contains inputted keyword Upcase" do
-          expect(@page.search_results.first.description.text).to include(keyword.capitalize)
+          expect(@page.first_result_description).to include(keyword.capitalize)
         end
       end # context upcase
 
@@ -58,29 +53,29 @@ RSpec.feature "Verify query search results" do
         given(:keyword) {'mONSteR'}
 
         scenario "Title contains inputted keyword Upcase" do
-          expect(@page.search_results.first.title.text).to include(keyword.capitalize)
+          expect(@page.first_result_title).to include(keyword.capitalize)
         end
 
         scenario "URL contains inputted keyword Downcase" do
-          expect(@page.search_results.first.url.text).to include(keyword.downcase)
+          expect(@page.first_result_url).to include(keyword.downcase)
         end
 
         scenario "Description contains inputted keyword Upcase" do
-          expect(@page.search_results.first.description.text).to include(keyword.capitalize)
+          expect(@page.first_result_description).to include(keyword.capitalize)
         end
       end # context mixed
     end # context ignore
 
 
     context "cyryllic symbols" do
-      given(:keyword) {'Монстр'}
+      given(:keyword) {'Колобок'}
 
       scenario "Title contains inputted cyryllic keyword" do
-        expect(@page.search_results.first.title.text).to include(keyword.capitalize)
+        expect(@page.first_result_title).to include(keyword.capitalize)
       end
 
       scenario "Description contains inputted cyryllic keyword" do
-        expect(@page.search_results.first.description.text).to include(keyword.capitalize)
+        expect(@page.first_result_description).to include(keyword.capitalize)
       end
     end
   end # feature one-word
@@ -90,11 +85,11 @@ RSpec.feature "Verify query search results" do
     given(:keyword) {'Buy Car Online'}
 
     scenario "Title contains inputted keyword" do
-      expect(@page.search_results.first.title.text).to include('Buy', 'Car', 'Online')
+      expect(@page.first_result_title).to include('Buy', 'Car', 'Online')
     end
 
     scenario "Description contains inputted keyword" do
-      expect(@page.search_results.first.description.text).to include('buy', 'car', 'online')
+      expect(@page.first_result_description).to include('buy', 'car', 'online')
     end
   end
 end # feature verify search
